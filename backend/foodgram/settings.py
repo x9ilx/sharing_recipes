0faa7dd269b99b0554aba243c2d498eda07bee2a
Владимир Бондаренко <AK23041989@live.com>
2024-03-19 20:28:57 +0600

    Add default colume for postgres

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,13 +1,10 @@
 import os
 from pathlib import Path
 
-from dotenv import read_dotenv
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-read_dotenv(BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
